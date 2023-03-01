3cc7b03f932fa88bc8aef04fe47f1ee8010375b9
Alex Salazar <helloworld3197@gmail.com>
2023-03-01 14:35:57 -0500

    Set up new user registration

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const dbConnect = require("./config/dbConnect");
 const app = express();
+const authRouter = require("./routes/authRoute");
 dbConnect();
 const PORT = process.env.PORT || 4000;
 
-app.use("/", (req, res) => {
-  res.send("Hello!");
-});
+app.use(express.json());
+app.use("/api/user", authRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
