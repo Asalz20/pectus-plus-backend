9b40bfeaf97297b672a3a98c47954ba061fe518d
Alex Salazar <helloworld3197@gmail.com>
2023-04-18 21:00:51 -0400

    Created getallProduct function to retrive all products from DB

--- controllers/productCtrl.js
+++ controllers/productCtrl.js
@@ -20,4 +20,13 @@ const getProduct = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { createProduct, getProduct };
+const getAllProduct = asyncHandler(async (req, res) => {
+  try {
+    const getAllProducts = await Product.find();
+    res.json(getAllProducts);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+module.exports = { createProduct, getProduct, getAllProduct };
