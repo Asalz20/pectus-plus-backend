a570c7e5ec9ff7d42c75a3be6db2eb7653b8d107
Alex Salazar <helloworld3197@gmail.com>
2023-04-20 23:07:58 -0400

    Created delete get and get all category(s)methods, routes

--- controllers/categoryCtrl.js
+++ controllers/categoryCtrl.js
@@ -13,6 +13,7 @@ const createCategory = asyncHandler(async (req, res) => {
 
 const updateCategory = asyncHandler(async (req, res) => {
   const { id } = req.params;
+  validateMongoDbId(id);
   try {
     const updateCategory = await Category.findByIdAndUpdate(id, req.body, {
       new: true,
@@ -23,4 +24,41 @@ const updateCategory = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { createCategory, updateCategory };
+const deleteCategory = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongoDbId(id);
+  try {
+    const deleteCategory = await Category.findByIdAndDelete(id);
+    res.json(deleteCategory);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const getCategory = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongoDbId(id);
+  try {
+    const getCategory = await Category.findById(id);
+    res.json(getCategory);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const getAllCategory = asyncHandler(async (req, res) => {
+  try {
+    const getAllCategory = await Category.find();
+    res.json(getAllCategory);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+module.exports = {
+  createCategory,
+  updateCategory,
+  deleteCategory,
+  getCategory,
+  getAllCategory,
+};
