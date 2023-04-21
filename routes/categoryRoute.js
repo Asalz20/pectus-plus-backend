b3b5abd3dbab1c87f4a8a542b7e905227fc0c49e
Alex Salazar <helloworld3197@gmail.com>
2023-04-20 22:46:27 -0400

    Created updateCategory method and route

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const { createCategory } = require("../controllers/categoryCtrl");
+const {
+  createCategory,
+  updateCategory,
+} = require("../controllers/categoryCtrl");
 const { authMiddleware, isAdmin } = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.post("/", authMiddleware, isAdmin, createCategory);
+router.post("/:id", authMiddleware, isAdmin, updateCategory);
 
 module.exports = router;
